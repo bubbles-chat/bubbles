6dbfcfd84bc84fbc1ac4c9c26a603f16143b6141
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-10-01 18:59:54 +0300

    added validation messgae text

--- components/CustomTextInput.tsx
+++ components/CustomTextInput.tsx
@@ -1,28 +1,33 @@
 import { useThemeColor } from '@/hooks/useThemeColor'
 import { CustomTextInputProps } from '@/types/types'
 import React from 'react'
-import { StyleSheet, View, TextInput } from 'react-native'
+import { StyleSheet, View, TextInput, Text } from 'react-native'
 
 const CustomTextInput: React.FC<CustomTextInputProps> = (props) => {
     const borderColor = useThemeColor({ light: props.lightBorderColor, dark: props.darkBorderColor }, 'text') as string
 
     return (
-        <View style={[
-            styles.container,
-            props.style,
-            {
-                borderColor: props.hasValidation ?
-                    props.state.validation?.isValid ? borderColor : 'red' : borderColor
-            }
-        ]}>
-            {props.Icon}
-            <TextInput
-                {...props}
-                style={[styles.textInput, { color: borderColor }]}
-                value={props.state.value}
-                placeholderTextColor={borderColor}
-            />
-        </View>
+        <>
+            <View style={[
+                styles.container,
+                props.style,
+                {
+                    borderColor: props.hasValidation ?
+                        props.state.validation?.isValid ? borderColor : 'red' : borderColor
+                }
+            ]}>
+                {props.Icon}
+                <TextInput
+                    {...props}
+                    style={[styles.textInput, { color: borderColor }]}
+                    value={props.state.value}
+                    placeholderTextColor={borderColor}
+                />
+            </View>
+            {(props.hasValidation && !props.state.validation?.isValid) && (
+                <Text style={styles.message}>{props.state.validation?.message}</Text>
+            )}
+        </>
     )
 }
 
@@ -40,5 +45,8 @@ const styles = StyleSheet.create({
     textInput: {
         marginLeft: 8,
         flex: 1
+    },
+    message: {
+        color: 'red'
     }
 })
