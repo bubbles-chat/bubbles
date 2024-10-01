0bf3a00a0d330968f364ffb1bc357214223684ae
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-10-01 18:59:06 +0300

    added authenticated user tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Tabs } from 'expo-router'
+
+const TabLayout = () => {
+  return (
+    <Tabs>
+        <Tabs.Screen name='index' />
+    </Tabs>
+  )
+}
+
+export default TabLayout
