913eaeb0e7787aa6932d0b458996285896d51b55
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-10-05 16:44:26 +0300

    created user slice

---
+++ store/userSlice.ts
@@ -0,0 +1,21 @@
+import { User } from "@/models/User";
+import { createSlice } from "@reduxjs/toolkit";
+
+interface UserState {
+    user: User | null
+}
+
+const initialState: UserState = {
+    user: null
+}
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {}, // the reducers property is required by typescript even if it is empty
+    extraReducers: (builder) => {
+
+    }
+})
+
+export default userSlice.reducer
