9293213e8fa1d9592b673c6473339c24cf1f59ef
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-10-11 17:54:26 +0300

    feat: controlling the visibilt of custom button background
    - added hasBackground prop to customButtonProps
    - background behaviour is determined based on the hasBackground prop

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,11 +4,19 @@ import { CustomButtonProps } from '@/types/types'
 import { ThemedText } from './ThemedText'
 import { useThemeColor } from '@/hooks/useThemeColor'
 
-const CustomButton: React.FC<CustomButtonProps> = (props) => {
+const CustomButton: React.FC<CustomButtonProps> = ({ hasBackground = true, ...props }) => {
     const background = useThemeColor({ dark: props.darkBackground, light: props.lightBackground }, 'buttonBackground') as string
 
     return (
-        <TouchableOpacity style={[styles.container, { backgroundColor: background }, props.style]} {...props}>
+        <TouchableOpacity
+            style={[
+                styles.container,
+                hasBackground && { backgroundColor: background },
+                !hasBackground && { borderWidth: 1, borderColor: background },
+                props.style
+            ]}
+            {...props}
+        >
             {props.Icon && <View style={styles.iconView}>
                 {props.Icon}
             </View>}
