9e3b755d40d0767fe35373d6bb4f5a1232536c8e
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-10-11 16:31:14 +0300

    fix: add missing documnet id property

--- models/User.model.ts
+++ models/User.model.ts
@@ -2,9 +2,9 @@ import Chat from "./Chat.model"
 
 export default interface User {
     _id: string,
-    displayName: string,
-    email: string,
-    chats: Chat[] | string[],
-    connections: User[] | string[],
-    photoURL: string,
+    displayName: string
+    email: string
+    chats: Chat[] | string[]
+    connections: User[] | string[]
+    photoURL: string
 }
