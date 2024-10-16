deb61c60db3204aa30ade572f47227d50054fd70
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-10-16 23:19:45 +0300

    update: changed background color for dark theme

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -21,7 +21,7 @@ export const Colors = {
   },
   dark: {
     text: '#ECEDEE',
-    background: '#09203F',
+    background: '#000',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
