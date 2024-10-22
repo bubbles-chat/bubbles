e02199f7439968381f1b7cefe86b0d7ef9bd3f30
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-10-22 23:28:25 +0300

    fix: handled token expiration
    - calling updateAuthHeaders function before making any API calls

--- api/requestApi.ts
+++ api/requestApi.ts
@@ -1,27 +1,34 @@
 import Request from "@/models/Request.model";
 import { AxiosResponse } from "axios";
 import client from "./client";
+import { updateAuthHeaders } from "@/utils/jwt";
 
 export async function addRequest(reveiver: string): Promise<AxiosResponse<{ message: string, request?: Request }>> {
+    await updateAuthHeaders()
     return await client.post(`/request/addRequest/${reveiver}`)
 }
 
 export async function getSentRequests(): Promise<AxiosResponse<{ message: string, requests?: Request[] }>> {
+    await updateAuthHeaders()
     return await client.get('/request/getSentRequests')
 }
 
 export async function cancelRequest(id: string): Promise<AxiosResponse<{ message: string, request?: Request }>> {
+    await updateAuthHeaders()
     return await client.delete(`/request/deleteRequest/${id}`)
 }
 
 export async function getPendingRequests(): Promise<AxiosResponse<{ message: string, requests?: Request[] }>> {
+    await updateAuthHeaders()
     return await client.get('/request/getPendingRequests')
 }
 
 export async function rejectRequest(id: string): Promise<AxiosResponse<{ message: string, request?: Request }>> {
+    await updateAuthHeaders()
     return await client.put(`/request/rejectRequest/${id}`)
 }
 
 export async function acceptRequest(id: string): Promise<AxiosResponse<{ message: string, request?: Request }>> {
+    await updateAuthHeaders()
     return await client.put(`/request/acceptRequest/${id}`)
 }
