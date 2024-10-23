b8919d9eea4dc025fce16ae7af8f907b08c12d67
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-10-23 22:12:36 +0300

    feat: fetching last message

--- components/ChatFlatListItem.tsx
+++ components/ChatFlatListItem.tsx
@@ -8,9 +8,11 @@ import User from '@/models/User.model'
 import { ThemedText } from './ThemedText'
 import { Colors } from '@/constants/Colors'
 import { router } from 'expo-router'
+import { getMessage } from '@/api/messageApi'
 
 const ChatFlatListItem = ({ item }: { item: Chat }) => {
     const { user } = useAppSelector(state => state.user)
+    const [lastMessage, setLastMessage] = useState('')
     const colorScheme = useColorScheme()
     const textColor = colorScheme === 'dark' ? Colors.dark.icon : Colors.light.icon
 
@@ -27,13 +29,28 @@ const ChatFlatListItem = ({ item }: { item: Chat }) => {
                 }
             } catch (e) {
                 const err = e as AxiosError
+                console.log('ChatListItem:fetchOtherUser:', err.response?.data);
+            }
+        }
 
-                console.log('ChatListItem:fetchUser:', err.response?.data);
+        const fetchLastMessage = async () => {
+            try {
+                if (item.lastMessage) {
+                    const response = await getMessage(item.lastMessage as string)
+
+                    if (response.status === 200) {
+                        setLastMessage(response.data.message.text)
+                    }
+                }
+            } catch (e) {
+                const err = e as AxiosError
+                console.log('ChatListItem:fetchLastMessage:', err.response?.data);
             }
         }
 
         useEffect(() => {
             fetchOtherUser()
+            fetchLastMessage()
         }, [])
 
         return (
@@ -52,7 +69,7 @@ const ChatFlatListItem = ({ item }: { item: Chat }) => {
                 />
                 <View>
                     <ThemedText>{otherUser?.displayName}</ThemedText>
-                    <Text style={{ color: textColor }}>Placeholder for last message</Text>
+                    <Text style={{ color: textColor }}>{lastMessage}</Text>
                 </View>
             </Pressable>
         )
