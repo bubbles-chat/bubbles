72c0183097bbbbeba342c593df125c60885e4752
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-10-23 17:14:12 +0300

    update: changed timestamps type to string

--- models/Message.model.ts
+++ models/Message.model.ts
@@ -2,11 +2,11 @@ import Chat from "./Chat.model";
 import User from "./User.model";
 
 export default interface Message {
-    _id: string
+    _id?: string
     chatId: Chat | string
     sender: User | string
     text: string
     attachmentsUrl: string[]
-    createdAt: Date
-    updatedAt: Date
+    createdAt?: string
+    updatedAt?: string
 }
