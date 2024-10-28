3209c8cd32374b1febddeb51bc0ea6d3844f4ac3
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-10-29 00:39:44 +0300

    update: changed item type to DocumentPickerAsset

--- components/AttachmentPreviewFlatListItem.tsx
+++ components/AttachmentPreviewFlatListItem.tsx
@@ -2,17 +2,18 @@ import AudioPreviewFlatListItem from './AudioPreviewFlatListItem'
 import DocumentPreviewFlatListItem from './DocumentPreviewFlatListItem'
 import ImagePreviewFlatListItem from './ImagePreviewFlatListItem'
 import VideoPreviewFlatListItem from './VideoPreviewFlatListItem'
+import * as DocumentPicker from 'expo-document-picker'
 
-const AttachmentPreviewFlatListItem = ({ item, onPress }: { item: { uri: string, type: string, name: string }, onPress: () => any }) => {
-    if (item.type.includes('image')) {
+const AttachmentPreviewFlatListItem = ({ item, onPress }: { item: DocumentPicker.DocumentPickerAsset, onPress: () => any }) => {
+    if (item.mimeType?.includes('image')) {
         return <ImagePreviewFlatListItem name={item.name} uri={item.uri} onPress={onPress} />
     }
 
-    if (item.type.includes('video')) {
+    if (item.mimeType?.includes('video')) {
         return <VideoPreviewFlatListItem name={item.name} uri={item.uri} onPress={onPress} />
     }
 
-    if (item.type.includes('audio')) {
+    if (item.mimeType?.includes('audio')) {
         return <AudioPreviewFlatListItem name={item.name} onPress={onPress} uri={item.uri} />
     }
 
