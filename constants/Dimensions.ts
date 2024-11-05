b3b3d3dc866e27c7ec8b45ed156e9b748aed25ff
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-11-05 02:04:38 +0200

    feat: add horizontal padding, attachments list gap, and number of columns of attachments list constants

--- constants/Dimensions.ts
+++ constants/Dimensions.ts
@@ -2,3 +2,6 @@ export const PADDING_TOP = 16
 export const PADDING_BOTTOM = 8
 export const TOP_BAR_HEIGHT = 50
 export const TAB_BAR_HEIGHT = 70
+export const PADDING_HORIZONTAL = 8
+export const ATTACHMENTS_LIST_GAP = 4
+export const ATTACHMENTS_LIST_NUM_COLUMN = 3
