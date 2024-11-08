dd6c4fc1856d50dbeb66b30e637cd949f6b7e89a
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-11-08 21:04:58 +0200

    feat: added "isChatArray" function

--- utils/typeChecker.ts
+++ utils/typeChecker.ts
@@ -1,3 +1,4 @@
+import Chat from "@/models/Chat.model";
 import User from "@/models/User.model";
 
 export const isUser = (user: User | string): user is User => {
@@ -7,3 +8,7 @@ export const isUser = (user: User | string): user is User => {
 export const isUserArray = (connections: User[] | string[]): connections is User[] => {
     return connections.length > 0 && typeof connections[0] !== 'string'
 }
+
+export const isChatArray = (chats: Chat[] | string[]): chats is Chat[] => {
+    return chats.length > 0 && typeof chats[0] !== 'string'
+  }
