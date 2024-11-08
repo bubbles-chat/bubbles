63ced307aee49a9d5e0c4cab9d100b8238aab71d
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-11-08 17:46:38 +0200

    update: update auth headers before making chat API calls

--- api/chatApi.ts
+++ api/chatApi.ts
@@ -2,17 +2,30 @@ import { AxiosResponse } from "axios";
 import client from "./client";
 import Chat from "@/models/Chat.model";
 import User from "@/models/User.model";
+import { updateAuthHeaders } from "@/utils/jwt";
 
 export async function createGroupChat(chatName: string): Promise<AxiosResponse<{
     message: string,
     chat: Chat,
     user: User
 }>> {
+    await updateAuthHeaders()
     return await client.post('/chat/createGroupChat', {
         chatName
     })
 }
 
-export async function addUserToGroupChat(chatId:string, userId: string): Promise<AxiosResponse<{message: string, chat: Chat}>> {
+export async function addUserToGroupChat(chatId: string, userId: string): Promise<AxiosResponse<{ message: string, chat: Chat }>> {
+    await updateAuthHeaders()
     return await client.put(`/chat/addUserToGroupChat/${userId}/${chatId}`)
+}
+
+export async function removeParticipantFromGroupChat(chatId: string, userId: string): Promise<
+    AxiosResponse<{
+        message: string,
+        chat: Chat
+    }>
+> {
+    await updateAuthHeaders()
+    return await client.put(`/chat/removeParticipant/${userId}/${chatId}`)
 }
