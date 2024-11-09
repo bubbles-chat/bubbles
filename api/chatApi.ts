104c84a78ffa042ad6c1783f3d6c8aa687d76298
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-11-10 00:05:49 +0200

    feat: add "changeGroupChatPhoto" API function

--- api/chatApi.ts
+++ api/chatApi.ts
@@ -1,8 +1,9 @@
-import { AxiosResponse } from "axios";
+import axios, { AxiosResponse } from "axios";
 import client from "./client";
 import Chat from "@/models/Chat.model";
 import User from "@/models/User.model";
 import { updateAuthHeaders } from "@/utils/jwt";
+import auth from '@react-native-firebase/auth'
 
 export async function createGroupChat(chatName: string): Promise<AxiosResponse<{
     message: string,
@@ -38,4 +39,22 @@ export async function makeParticipantAnAdmin(chatId: string, userId: string): Pr
 > {
     await updateAuthHeaders()
     return await client.put(`/chat/changeParticipantRoleToAdmin/${userId}/${chatId}`)
+}
+
+export async function changeGroupChatPhoto(data: FormData, chatId: string): Promise<AxiosResponse<{
+    message: string,
+    data?: {
+        url: string,
+        public_id: string,
+        type: string
+    }
+}>> {
+    const token = await auth().currentUser?.getIdToken()
+
+    return await axios.put(`${process.env.EXPO_PUBLIC_API_URL}/chat/changeGroupChatPhoto/${chatId}`, data, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+            "Authorization": `Bearer ${token}`
+        }
+    })
 }
