d8182f72fd94fe2010a837b666728d97d7b525bb
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-11-16 23:45:28 +0200

    update: utilize newly added "customBackgroundColor" and "customTextColor" props

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,26 +1,34 @@
-import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, TouchableOpacity, useColorScheme, View } from 'react-native'
 import React from 'react'
 import { CustomButtonProps } from '@/types/types'
 import { ThemedText } from './ThemedText'
 import { useThemeColor } from '@/hooks/useThemeColor'
+import { Colors } from '@/constants/Colors'
 
-const CustomButton: React.FC<CustomButtonProps> = ({ hasBackground = true, ...props }) => {
+const CustomButton: React.FC<CustomButtonProps> = ({ customBackgroundColor, customTextColor, hasBackground = true, style, ...props }) => {
     const background = useThemeColor({ dark: props.darkBackground, light: props.lightBackground }, 'buttonBackground') as string
+    const colorScheme = useColorScheme()
+    const text = colorScheme === 'dark' ? Colors.light.text : Colors.dark.text
 
     return (
         <TouchableOpacity
             style={[
                 styles.container,
-                hasBackground && { backgroundColor: background },
-                !hasBackground && { borderWidth: 1, borderColor: background },
-                props.style
+                hasBackground && { backgroundColor: customBackgroundColor ? customBackgroundColor : background },
+                !hasBackground && { borderWidth: 1, borderColor: customBackgroundColor ? customBackgroundColor : background },
+                style
             ]}
             {...props}
         >
             {props.Icon && <View style={styles.iconView}>
                 {props.Icon}
             </View>}
-            <ThemedText style={styles.text}>{props.text}</ThemedText>
+            <ThemedText style={[
+                styles.text,
+                {
+                    color: customTextColor ? customTextColor : hasBackground ? text : background
+                }
+            ]}>{props.text}</ThemedText>
         </TouchableOpacity>
     )
 }
